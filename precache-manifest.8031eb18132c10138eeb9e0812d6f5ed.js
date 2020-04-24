10ba4a473f5263efc8be4e85d3d41941e4b6c22e
mopixels <m.rimkus93@gmail.com>
2020-04-24 21:43:00 +0300

    Updates

---
+++ precache-manifest.8031eb18132c10138eeb9e0812d6f5ed.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "01472d0395b988a244d37c7164fe9d1a",
+    "url": "/addresser/index.html"
+  },
+  {
+    "revision": "fa99c3ff7be76e9ac8ba",
+    "url": "/addresser/static/css/main.5143acd4.chunk.css"
+  },
+  {
+    "revision": "9f3da24ca904b86dad41",
+    "url": "/addresser/static/js/2.ebc32b5e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/addresser/static/js/2.ebc32b5e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fa99c3ff7be76e9ac8ba",
+    "url": "/addresser/static/js/main.bc401aa4.chunk.js"
+  },
+  {
+    "revision": "2b87b9bca1e39a756d9b",
+    "url": "/addresser/static/js/runtime-main.eecdb1bc.js"
+  },
+  {
+    "revision": "2ca6cac9835098b3eadfb326eee25207",
+    "url": "/addresser/static/media/Cocogoose Pro Regular Trial.2ca6cac9.woff"
+  }
+]);
